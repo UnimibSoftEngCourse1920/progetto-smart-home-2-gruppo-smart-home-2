4ee45e51de1a7a42b70ea7a659f3e8aec712f94d
Alessandra Rota <a.rota54@campus.unimib.it>
2020-01-15 11:27:40 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module smarthome2 {
+	exports application;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.web;
+}
