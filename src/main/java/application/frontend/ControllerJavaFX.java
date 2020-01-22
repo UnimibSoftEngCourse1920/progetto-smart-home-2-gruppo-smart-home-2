712b1cab460e1bb0d832eb7e7f54d5bd1fa5afb4
Alessandra Rota <a.rota54@campus.unimib.it>
2020-01-22 16:02:40 +0100

    Creato stanze.fxml e modificato homepage.fxml e menu.fxml

--- src/main/java/application/frontend/ControllerJavaFX.java
+++ src/main/java/application/frontend/ControllerJavaFX.java
@@ -19,18 +19,16 @@ public class ControllerJavaFX {
 	@FXML
 	public BorderPane borderPaneProgrammi;
 	public ControllerCasa c = new ControllerCasa(this);
-	
 
 	public void start(Stage primaryStage) throws Exception {
     	try {
     		primaryStage.setTitle("Smart-Home-2");
-    		Parent root = FXMLLoader.load(getClass().getResource("views/dashboard.fxml"));
-    		Scene home = new Scene(root, 600,600);
+    		Parent root = FXMLLoader.load(getClass().getResource("views/menu.fxml"));
+    		Scene home = new Scene(root);
     		home.getStylesheets().add("application.css");
     		primaryStage.setScene(home);
 			primaryStage.show();
-			
-			
+			primaryStage.setMaximized(true);
     	} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -38,8 +36,20 @@ public class ControllerJavaFX {
 
 	@FXML
 	public void viewProgrammi() throws IOException {
-		Parent programma = FXMLLoader.load(getClass().getResource("views/programmi.fxml"));
-		borderPaneProgrammi.setCenter(programma);
+		Parent programmi = FXMLLoader.load(getClass().getResource("views/programmi.fxml"));
+		borderPaneProgrammi.setCenter(programmi);
+	}
+	
+	@FXML
+	public void viewHomePage() throws IOException {
+		Parent homepage = FXMLLoader.load(getClass().getResource("views/homepage.fxml"));
+		borderPaneProgrammi.setCenter(homepage);
+	}
+	
+	@FXML
+	public void viewStanze() throws IOException {
+		Parent homepage = FXMLLoader.load(getClass().getResource("views/stanze.fxml"));
+		borderPaneProgrammi.setCenter(homepage);
 	}
 	
 	@FXML
