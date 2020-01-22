2d7cf30af9c6ff5ef858bce022381e86dc7ce48a
Alessandra Rota <a.rota54@campus.unimib.it>
2020-01-22 12:39:06 +0100

    Add programmi.fxml, dashboard.fxml

--- src/main/java/application/frontend/ControllerJavaFX.java
+++ src/main/java/application/frontend/ControllerJavaFX.java
@@ -8,22 +8,28 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class ControllerJavaFX {
 	
 	@FXML
-	public Button button;
+	public BorderPane borderPaneProgrammi;
+	public ControllerCasa c = new ControllerCasa(this);
+	
 
 	public void start(Stage primaryStage) throws Exception {
     	try {
     		primaryStage.setTitle("Smart-Home-2");
-    		Parent root = FXMLLoader.load(getClass().getResource("views/esempio.fxml"));
+    		Parent root = FXMLLoader.load(getClass().getResource("views/dashboard.fxml"));
     		Scene home = new Scene(root, 600,600);
     		home.getStylesheets().add("application.css");
     		primaryStage.setScene(home);
 			primaryStage.show();
+			
 			
     	} catch (Exception e) {
 			e.printStackTrace();
@@ -31,7 +37,21 @@ public class ControllerJavaFX {
     }
 
 	@FXML
-	public void click() throws IOException {
-		System.out.println("ciao2");
+	public void viewProgrammi() throws IOException {
+		Parent programma = FXMLLoader.load(getClass().getResource("views/programmi.fxml"));
+		borderPaneProgrammi.setCenter(programma);
 	}
+	
+	@FXML
+	public void click() throws IOException {
+		//System.out.println("ciao");
+		Alert a1 = new Alert(Alert.AlertType.ERROR);
+		a1.setTitle("ciao");
+		a1.showAndWait();
+	}
+	
+	public ControllerCasa getControllerCasa() {
+		return this.c;
+	}
+	
 }
