9f3e00c376e7a5d0e836efa8f2c62ca5037e0397
Alessandra Rota <a.rota54@campus.unimib.it>
2020-01-24 11:49:26 +0100

    Inizializzazione JFrame

--- src/main/java/application/frontend/Main.java
+++ src/main/java/application/frontend/Main.java
@@ -1,8 +1,82 @@
 package application.frontend;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JTabbedPane;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+
+public class Main extends JFrame {
+
+	private JPanel contentPane;
+	private JPanel panelMenu;
+	private JPanel panelPrincipale;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+					frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 450);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		creazioneContentPane();
+	}
+
+	public void creazioneContentPane() {
+		panelMenu = new JPanel();
+		panelPrincipale = new JPanel();
+		panelPrincipale.setBackground(Color.LIGHT_GRAY);
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addComponent(panelMenu, GroupLayout.PREFERRED_SIZE, 261, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panelPrincipale, GroupLayout.DEFAULT_SIZE, 563, Short.MAX_VALUE))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(panelPrincipale, GroupLayout.DEFAULT_SIZE, 403, Short.MAX_VALUE)
+				.addComponent(panelMenu, GroupLayout.DEFAULT_SIZE, 403, Short.MAX_VALUE)
+		);
+		contentPane.setLayout(gl_contentPane);
+		
+		gestionePanelMenu();
+	}
 	
-import application.frontend.*;
-
-
-public class Main{
-
+	public void gestionePanelMenu() {
+	}
 }
