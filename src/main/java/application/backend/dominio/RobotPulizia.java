71bc38a13da0fbba9179c53398194aab9bcfbbf1
BeatriceStropeni <56863669+BeatriceStropeni@users.noreply.github.com>
2020-01-27 16:03:46 +0100

    Aggiunta funzione di aggiornamento della posizione del robot in funzione

--- src/main/java/application/backend/dominio/RobotPulizia.java
+++ src/main/java/application/backend/dominio/RobotPulizia.java
@@ -1,16 +1,25 @@
-
 package application.backend.dominio;
 
-public class RobotPulizia {
+import java.util.ArrayList;
+import java.util.TimerTask;
+
+import application.controllers.ControllerCasa;
+
+
+public class RobotPulizia extends TimerTask{
 
 	private boolean isInFunzione;
 	private Stanza base;
 	private Stanza posizione;
+	private ControllerCasa casa;
+	private ArrayList<Stanza> mappa;
 	
-	public RobotPulizia(Stanza base) {
+	public RobotPulizia(Stanza base, ControllerCasa casa) {
 		this.isInFunzione = false;
 		this.base = base;
 		this.posizione = base;
+		this.casa = casa;
+		mappa = (ArrayList<Stanza>) casa.getStanze();
 	}
 	
 	public Stanza getBase() {
@@ -33,12 +42,17 @@ public class RobotPulizia {
 		else 
 			this.isInFunzione = true;
 	}
-	
-	public void muovi(Stanza s) {
-		this.posizione = s;
-	}
-	
-	public void fine() {
-		this.posizione = base;
+
+	@Override
+	public void run() {
+		if((mappa.isEmpty()) || (mappa.size() == 1 && mappa.get(0) == base)) {
+			this.cambiaStato();
+			mappa = (ArrayList<Stanza>) casa.getStanze();
+			casa.spegniRobot();
+		}
+		else
+			if(!(mappa.get(0) == base)) 
+					this.posizione = mappa.get(0);
+				mappa.remove(0);
 	}
 }
