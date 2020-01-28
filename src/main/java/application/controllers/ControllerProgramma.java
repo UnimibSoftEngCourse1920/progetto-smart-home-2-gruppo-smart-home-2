9ebeeba2248f86830ad722cc993f19a5c9dbb8eb
BeatriceStropeni <56863669+BeatriceStropeni@users.noreply.github.com>
2020-01-28 10:01:54 +0100

    Correzione bug

--- src/main/java/application/controllers/ControllerProgramma.java
+++ src/main/java/application/controllers/ControllerProgramma.java
@@ -75,7 +75,7 @@ public class ControllerProgramma {
 		for (Programma p : programmi) {
 			if(p instanceof ProgrammaGiornaliero) {
 				inizio = ((ProgrammaGiornaliero) p).getInizio();
-				if(inizio.compareTo(ora)==0 || inizio.compareTo(ora)== -1) {
+				if(inizio.compareTo(ora)==0 || inizio.compareTo(ora) <0) {
 					if(p.getElemento() instanceof SensoreTemperatura)
 						cambiaTemperatura(((ProgrammaGiornaliero) p).getValoreDiSetting(), (SensoreTemperatura) p.getElemento());
 					else
@@ -84,7 +84,7 @@ public class ControllerProgramma {
 				}
 			if (p instanceof ProgrammaSettimanale){
 				inizio= ((ProgrammaSettimanale) p).getInizio(giorno);
-				if(inizio.compareTo(ora)==0 || inizio.compareTo(ora)== -1) {
+				if(inizio.compareTo(ora)==0 || inizio.compareTo(ora) <0) {
 					if(p.getElemento() instanceof SensoreTemperatura)
 						cambiaTemperatura(((ProgrammaSettimanale) p).getValoreDiSetting(giorno), (SensoreTemperatura) p.getElemento());
 					else
@@ -103,7 +103,7 @@ public class ControllerProgramma {
 		for (Programma p : programmi) {
 			if(p instanceof ProgrammaSettimanale) {
 				fine = ((ProgrammaSettimanale) p).getFine(giorno);
-				if(fine != null && (fine.compareTo(ora)==0 || fine.compareTo(ora)== -1)) {
+				if(fine != null && (fine.compareTo(ora)==0 || fine.compareTo(ora)<0)) {
 					if(p.getElemento() instanceof SensoreTemperatura)
 						cambiaTemperatura(SensoreTemperatura.TEMPERATURADEFAULT, (SensoreTemperatura) p.getElemento());
 					else
@@ -112,7 +112,7 @@ public class ControllerProgramma {
 			}
 			else if(p instanceof ProgrammaGiornaliero) {
 				fine = ((ProgrammaGiornaliero) p).getFine();
-				if(fine != null && (fine.compareTo(ora)==0 || fine.compareTo(ora)== -1)) {
+				if(fine != null && (fine.compareTo(ora)==0 || fine.compareTo(ora) <0)) {
 					if(p.getElemento() instanceof SensoreTemperatura)
 						cambiaTemperatura(SensoreTemperatura.TEMPERATURADEFAULT, (SensoreTemperatura) p.getElemento());
 					else
