d8d6d74e1998a4c79b19bc6fe036c2a5754f046e
BeatriceStropeni <56863669+BeatriceStropeni@users.noreply.github.com>
2020-10-15 08:59:20 +0200

    Aggiunta primo test

---
+++ src/test/java/application/controllers/TestControllerCasa.java
@@ -0,0 +1,14 @@
+package application.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestControllerCasa {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
