d46cf4987f60d93d447656fea2a1ac537f8d330d
BeatriceStropeni <56863669+BeatriceStropeni@users.noreply.github.com>
2020-10-15 09:24:52 +0200

    Aggiunta test

--- src/test/java/application/controllers/TestControllerCasa.java
+++ src/test/java/application/controllers/TestControllerCasa.java
@@ -1,14 +1,25 @@
 package application.controllers;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import application.backend.dominio.*;
 
 public class TestControllerCasa {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAddStanza() {
+		ControllerCasa c= new ControllerCasa(null);
+		Stanza s=new Stanza("prova");
+		c.addStanza(s);
+		assertEquals(s.getNome(),c.getStanza("prova").getNome());
 	}
 
+	@Test
+	public void testAddRobot() {
+		ControllerCasa c= new ControllerCasa(null);
+		Stanza s=new Stanza("prova");
+		c.addStanza(s);
+		c.addRobot(s);
+		assertTrue(c.getRobot()!=null);
+	}
 }
